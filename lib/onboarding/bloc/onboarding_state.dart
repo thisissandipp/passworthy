3a5741d2d27dacdf285b6f0debd3d26dd83400a8
Sandip <sandipramanik07@gmail.com>
2025-02-17 00:51:50 +0530

    chore: upgrade analysis options

--- lib/onboarding/bloc/onboarding_state.dart
+++ lib/onboarding/bloc/onboarding_state.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'onboarding_bloc.dart';
 
 class OnboardingState extends Equatable {
--- test/onboarding/bloc/onbarding_event_test.dart
+++ test/onboarding/bloc/onbarding_event_test.dart
@@ -1,3 +1,4 @@
+// Ignore below specifics for testing environment.
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
