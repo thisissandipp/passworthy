bf7b981acdf17341c5bba8083e0a3f0969148d0e
Sandip <sandipramanik07@gmail.com>
2025-03-01 20:56:29 +0530

    chore: add downloaded fonts and its license

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -20,11 +22,21 @@ class AppBlocObserver extends BlocObserver {
   }
 }
 
+/// Bootstrap is responsible for any common setup and calls
+/// [runApp] with the widget returned by [builder] in an error zone.
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  // Add Open Font License (OFL) for Inter google font
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/licenses/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  // Log all uncaught build phase errors from the framework
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
+  // Custom Bloc Observer
   Bloc.observer = const AppBlocObserver();
 
   runApp(await builder());
